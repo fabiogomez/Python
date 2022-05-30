e03169ecbd28b20f7e2c45b94102646f72e5bcf3
Fabio Gomez <fabiogomez28@gmail.com>
2022-05-30 12:43:12 -0500

    algoritms in python

---
+++ Platzi/POO_python/ordenamiento/mescla.py
@@ -0,0 +1,92 @@
+import random
+
+def ordenamiento_por_mezcla(lista):
+    if len(lista) > 1:
+        medio = len(lista) // 2
+        izquierda = lista[:medio]
+        derecha = lista[medio:]
+        print(izquierda, '*' * 5, derecha)
+
+        # llamada recursiva en cada mitad
+        ordenamiento_por_mezcla(izquierda)
+        ordenamiento_por_mezcla(derecha)
+
+        # Iteradores para recorrer las dos sublistas
+        i = 0
+        j = 0
+        # Iterador para la lista principal
+        k = 0
+
+        while i < len(izquierda) and j < len(derecha):
+            if izquierda[i] < derecha[j]:
+                lista[k] = izquierda[i]
+                i += 1
+            else:
+                lista[k] = derecha[j]
+                j += 1
+
+            k += 1
+
+        while i < len(izquierda):
+            lista[k] = izquierda[i]
+            i += 1
+            k +=1
+
+        while j < len(derecha):
+            lista[k] = derecha[j]
+            j += 1
+            k += 1
+        
+        print(f'izquierda {izquierda}, derecha {derecha}')
+        print(lista)
+        print('-' * 50)
+
+    return lista
+
+
+if __name__ == '__main__':
+    tamano_de_lista = int(input('De que tamano sera la lista? '))
+
+    lista = [random.randint(0, 100) for i in range(tamano_de_lista)]
+    print(lista)
+    print('-' * 20)
+
+    lista_ordenada = ordenamiento_por_mezcla(lista)
+    print(lista_ordenada)
+
+
+"""     
+i = o
+j=0
+k= 0
+
+len izq 1
+len der 1
+
+[1 ] 
+j = 1
+k = 1
+[1 3]
+i =1
+k = 2
+
+[1 3] [ 6 8 ]
+i = 0
+j = 0
+k = 0
+
+len iz (2)
+len der (2)
+[1 ]
+i = 1
+k=1
+[1 3] 
+i=2
+k=2
+[1 3 6]
+j=1
+k=3
+[1 3 6 8]
+j=2
+ """
+
